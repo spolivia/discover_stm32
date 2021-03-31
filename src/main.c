e734e5eba06fc15586078c2190b186f8ef285714
Olivia SP <olivia.serenelli.pesin@gmail.com>
2021-03-31 16:31:47 +0200

    First commit with basic implementation

---
+++ src/main.c
@@ -0,0 +1,74 @@
+#include <stdint.h>
+
+#define     __IO    volatile
+#define     __IOM   volatile
+
+#define PERIPH_BASE 0x40000000UL
+#define AHB1PERIPH_BASE (PERIPH_BASE + 0x00020000UL)
+#define GPIOD_BASE (AHB1PERIPH_BASE + 0x0C00UL)
+#define GPIOD ((GPIO_TypeDef *) GPIOD_BASE)
+
+#define RCC_BASE (AHB1PERIPH_BASE + 0x3800UL)
+#define RCC ((RCC_TypeDef *) RCC_BASE)
+
+typedef struct
+{
+  __IO uint32_t MODER;    
+  __IO uint32_t OTYPER;   
+  __IO uint32_t OSPEEDR;  
+  __IO uint32_t PUPDR;    
+  __IO uint32_t IDR;      
+  __IO uint32_t ODR;      
+  __IO uint32_t BSRR;     
+  __IO uint32_t LCKR;     
+  __IO uint32_t AFR[2];   
+} GPIO_TypeDef;
+
+typedef struct
+{
+  __IO uint32_t CR;            
+  __IO uint32_t PLLCFGR;       
+  __IO uint32_t CFGR;          
+  __IO uint32_t CIR;           
+  __IO uint32_t AHB1RSTR;      
+  __IO uint32_t AHB2RSTR;      
+  __IO uint32_t AHB3RSTR;      
+  uint32_t      RESERVED0;     
+  __IO uint32_t APB1RSTR;      
+  __IO uint32_t APB2RSTR;      
+  uint32_t      RESERVED1[2];  
+  __IO uint32_t AHB1ENR;       
+  __IO uint32_t AHB2ENR;       
+  __IO uint32_t AHB3ENR;       
+  uint32_t      RESERVED2;     
+  __IO uint32_t APB1ENR;       
+  __IO uint32_t APB2ENR;       
+  uint32_t      RESERVED3[2];  
+  __IO uint32_t AHB1LPENR;     
+  __IO uint32_t AHB2LPENR;     
+  __IO uint32_t AHB3LPENR;     
+  uint32_t      RESERVED4;     
+  __IO uint32_t APB1LPENR;     
+  __IO uint32_t APB2LPENR;     
+  uint32_t      RESERVED5[2];  
+  __IO uint32_t BDCR;          
+  __IO uint32_t CSR;           
+  uint32_t      RESERVED6[2];  
+  __IO uint32_t SSCGR;         
+  __IO uint32_t PLLI2SCFGR;    
+} RCC_TypeDef;
+ 
+
+int main()
+{
+ 
+    RCC->AHB1ENR |= 1<<3;
+    GPIOD->MODER |= 0x55000000;
+    GPIOD->ODR |= 0xF000; 
+
+    while(1);
+
+  return 0;
+}
+
+
